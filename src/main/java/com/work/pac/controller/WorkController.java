af6bc1775fb5e46bc83448bca2d6d2ce117246d1
kobayashi <cals.gearchange@gmail.com>
2024-11-15 11:21:47 +0900

    s

--- src/main/java/com/work/pac/controller/WorkController.java
+++ src/main/java/com/work/pac/controller/WorkController.java
@@ -1,13 +1,9 @@
 package com.work.pac.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.work.pac.dto.WorkRequest;
 import com.work.pac.dto.WorkUpdateRequest;
-import com.work.pac.entity.Work;
-import com.work.pac.service.WorkService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class WorkController {
 
 	@Autowired
-	private WorkService workService;
+//	private WorkService workService;
 
 	  /**
 	   * リスト画面を表示
@@ -36,8 +30,8 @@ public class WorkController {
 	   */
 	  @GetMapping("/work/list")
 	  public String displayList(Model model) {
-		  List<Work> worklist = workService.searchAll();
-		  model.addAttribute("worklist", worklist);
+//		  List<Work> worklist = workService.searchAll();
+//		  model.addAttribute("worklist", worklist);
 		  return "work/list";
 	  }
 
@@ -48,29 +42,29 @@ public class WorkController {
 	   */
 	  @GetMapping("/work/search")
 	  public String search(@RequestParam String name, String state, String priority, Model model) {
-		  List<Work> searchResult = workService.search(name, state, priority);
-		  model.addAttribute("searchResult", searchResult);
+//		  List<Work> searchResult = workService.search(name, state, priority);
+//		  model.addAttribute("searchResult", searchResult);
 		  return "work/search";
 	  }
 
 	  @GetMapping("/work/add")
 	  public String displayadd(Model model) {
-		  model.addAttribute("workRequest", new WorkRequest());
+//		  model.addAttribute("workRequest", new WorkRequest());
 		  return "work/add";
 	  }
 
 	  @PostMapping("/work/create")
 	  public String create(@Validated @ModelAttribute WorkRequest workRequest, BindingResult result, Model model) {
 		  
-		if (result.hasErrors()) {
-		  List<String> errorList = new ArrayList<String>();
-		  for (ObjectError error : result.getAllErrors()) {
-		    errorList.add(error.getDefaultMessage());
-		  }
-		  model.addAttribute("validationError", errorList);
-		  return "work/add";
-		}
-		workService.create(workRequest);
+//		if (result.hasErrors()) {
+//		  List<String> errorList = new ArrayList<String>();
+//		  for (ObjectError error : result.getAllErrors()) {
+//		    errorList.add(error.getDefaultMessage());
+//		  }
+//		  model.addAttribute("validationError", errorList);
+	//	  return "work/add";
+		//}
+//		workService.create(workRequest);
 		return "redirect:/work/list";
 	  }
 
@@ -81,8 +75,8 @@ public class WorkController {
 	   */
 	  @GetMapping("/work/{id}")
 	  public String displayView(@PathVariable Long id, Model model) {
-		  Work work = workService.findById(id);
-		  model.addAttribute("workData", work);
+//		  Work work = workService.findById(id);
+//		  model.addAttribute("workData", work);
 		  return "work/view";
 	  }
 
@@ -92,38 +86,38 @@ public class WorkController {
 	   */
 	  @GetMapping("/work/{id}/delete")
 	  public String delete(@PathVariable Long id, Model model) {
-		  workService.delete(id);
+//		  workService.delete(id);
 		  return "redirect:/work/list";
 	  }
 
 	  @GetMapping("/work/{id}/edit")
 	  public String displayEdit(@PathVariable Long id, Model model) {
-		  Work work = workService.findById(id);
-		  WorkUpdateRequest workUpdateRequest = new WorkUpdateRequest();
-		  workUpdateRequest.setId(work.getId());
-		  workUpdateRequest.setName(work.getName());
-		  workUpdateRequest.setState(work.getState());
-		  workUpdateRequest.setDeadline(work.getDeadline());
-		  workUpdateRequest.setPriority(work.getPriority());
-		  workUpdateRequest.setDetail(work.getDetail());
-		  model.addAttribute("workUpdateRequest", workUpdateRequest);
+//		  Work work = workService.findById(id);
+//		  WorkUpdateRequest workUpdateRequest = new WorkUpdateRequest();
+//		  workUpdateRequest.setId(work.getId());
+//		  workUpdateRequest.setName(work.getName());
+//		  workUpdateRequest.setState(work.getState());
+//		  workUpdateRequest.setDeadline(work.getDeadline());
+//		  workUpdateRequest.setPriority(work.getPriority());
+//		  workUpdateRequest.setDetail(work.getDetail());
+//		  model.addAttribute("workUpdateRequest", workUpdateRequest);
 		  return "work/edit";
 	  }
 	  
 	  @PostMapping("/work/update")
 	  public String update(@Validated @ModelAttribute WorkUpdateRequest workUpdateRequest, BindingResult result, Model model) {
+//
+//		  if (result.hasErrors()) {
+//			  List<String> errorList = new ArrayList<String>();
 
-		  if (result.hasErrors()) {
-			  List<String> errorList = new ArrayList<String>();
+//			  for (ObjectError error : result.getAllErrors()) {
+//				  errorList.add(error.getDefaultMessage());
+//			  }
+//			  model.addAttribute("validationError", errorList);
+//			  return "work/edit";
+//		  }
 
-			  for (ObjectError error : result.getAllErrors()) {
-				  errorList.add(error.getDefaultMessage());
-			  }
-			  model.addAttribute("validationError", errorList);
-			  return "work/edit";
-		  }
-
-		  workService.update(workUpdateRequest);
+//		  workService.update(workUpdateRequest);
 		  return String.format("redirect:/work/%d",workUpdateRequest.getId());
 		  
 	  }
